546dee4210a2b3b43cb73b273119fc12ec101ea6
Jean-Gabriel Gaudreault <jean-gabriel.gaudreault@hotmail.com>
2020-03-10 23:30:00 -0400

    Finished tutorial

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public ParticleSystem smokeEffect;
+    public float speed = 3.0f;
+    public float distance = 4.0f;
+
+    Rigidbody2D rigidbody2d;
+    float distanceTraveled = 0.0f;
+    bool isGoingUp = true;
+    bool broken = true;
+
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!broken)
+        {
+            return;
+        }
+
+        Vector2 position = rigidbody2d.position;
+
+        if (distanceTraveled < distance)
+        {
+            if (isGoingUp)
+            {
+                position.y = position.y + speed * Time.deltaTime; //Going up
+                animator.SetFloat("Move Y", 1);
+            }
+            else
+            {
+                position.y = position.y + speed * Time.deltaTime * -1; //Going down
+                animator.SetFloat("Move Y", -1);
+            }
+            distanceTraveled += speed * Time.deltaTime;
+        }
+        else
+        {
+            isGoingUp = !isGoingUp;
+            distanceTraveled = 0;
+        }
+
+        rigidbody2d.MovePosition(position);
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
+    }
+
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2d.simulated = false;
+        animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
+    }
+}
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -12,13 +12,27 @@ public class RubyController : MonoBehaviour
 
     public float speed = 3.0f;
 
+    public GameObject projectilePrefab;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float invincibleTimer;
+
+    AudioSource audioSource;
+
+    Vector2 lookDirection = new Vector2(1, 0);
+
+    Animator animator;
     Rigidbody2D rigidbody2d;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = 1;
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -27,16 +41,80 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * horizontal * Time.deltaTime;
-        position.y = position.y + speed * vertical * Time.deltaTime;
+
+        position = position + move * speed * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+
+        if(isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if(invincibleTimer <= 0)
+            {
+                isInvincible = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Launch();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(rigidbody2d.position + Vector2.up * 0.2f, lookDirection, 1.5f, LayerMask.GetMask("NPC"));
+
+            NonPlayerCharacter charac = hit.collider.GetComponent<NonPlayerCharacter>();
+
+            if(charac != null)
+            {
+                charac.DisplayDialog();
+            }
+        }
+    }
+
+    void Launch()
+    {
+        GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
+
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+
+        projectile.Launch(lookDirection, 300);
+
+        animator.SetTrigger("Launch");
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        audioSource.PlayOneShot(clip);
     }
 
     public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            if (isInvincible)
+                return;
+            animator.SetTrigger("Hit");
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
+        UIHealthBar.instance.SetValue(currentHealth / (float)maxHealth);
     }
 }
